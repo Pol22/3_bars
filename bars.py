c16e564f668a853a307682e18ebdc506d31d3dff
Pol22 <pashazor22@gmail.com>
2016-09-24 16:38:56 +0300

    Задача про бары Москвы

--- bars.py
+++ bars.py
@@ -1,21 +1,46 @@
 import json
-
+import sys
+import math
 
 def load_data(filepath):
-    pass
+    data=json.loads(open(filepath,encoding='utf-8').read())
+    return data
 
 
 def get_biggest_bar(data):
-    pass
-
+    maxSeats=0
+    for line in data:
+        if(line['Cells']['SeatsCount']>maxSeats):
+            max=line
+            maxSeats=max['Cells']['SeatsCount']
+    return max
 
 def get_smallest_bar(data):
-    pass
-
+    minSeats=float('inf')
+    for line in data:
+        if(line['Cells']['SeatsCount']<minSeats):
+            min=line
+            minSeats=min['Cells']['SeatsCount']
+    return min
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
+    rast=float('inf')
+    for line in data:
+        x=line['Cells']['geoData']['coordinates'][0]
+        y=line['Cells']['geoData']['coordinates'][1]
+        line_rast=math.sqrt((longitude-x)**2+(latitude-y)**2)
+        if(line_rast<rast):
+            closest=line
+            rast=line_rast
+    return closest
 
 if __name__ == '__main__':
-    pass
+    file=load_data('data.json')
+    big_bar=get_biggest_bar(file)
+    small_bar=get_smallest_bar(file)
+    print("Biggest bar in Moscow: ", big_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), big_bar['Cells']['SeatsCount'])
+    print("Smallest bar in Moscow: ", small_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), small_bar['Cells']['SeatsCount'])
+    x=float(input("Input longitude: "))
+    y=float(input("Input latitude: "))
+    closest_bar=get_closest_bar(file, x, y)
+    print("Nearest bar: ", closest_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), closest_bar['Cells']['geoData']['coordinates'][0], closest_bar['Cells']['geoData']['coordinates'][1])
