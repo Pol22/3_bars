a626e0d3cda881811a9f4ef62fed6a5d4baa1bba
Pol22 <pashazor22@gmail.com>
2016-09-25 18:32:26 +0300

    Style pep8 and new names variables + README

--- bars.py
+++ bars.py
@@ -1,46 +1,70 @@
 import json
 import sys
 import math
+import os
 
-def load_data(filepath):
-    data=json.loads(open(filepath,encoding='utf-8').read())
-    return data
+
+def load_json_data(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8') as file_handler:
+        return json.load(file_handler)
 
 
 def get_biggest_bar(data):
-    maxSeats=0
-    for line in data:
-        if(line['Cells']['SeatsCount']>maxSeats):
-            max=line
-            maxSeats=max['Cells']['SeatsCount']
-    return max
+    max_seats = 0
+    for json_line in data:
+        if(json_line['Cells']['SeatsCount'] > max_seats):
+            biggest_bar = json_line
+            max_seats = biggest_bar['Cells']['SeatsCount']
+    return biggest_bar
+
 
 def get_smallest_bar(data):
-    minSeats=float('inf')
-    for line in data:
-        if(line['Cells']['SeatsCount']<minSeats):
-            min=line
-            minSeats=min['Cells']['SeatsCount']
-    return min
+    min_seats = float('inf')
+    for json_line in data:
+        if(json_line['Cells']['SeatsCount'] < min_seats):
+            smallest_bar = json_line
+            min_seats = smallest_bar['Cells']['SeatsCount']
+    return smallest_bar
+
 
 def get_closest_bar(data, longitude, latitude):
-    rast=float('inf')
-    for line in data:
-        x=line['Cells']['geoData']['coordinates'][0]
-        y=line['Cells']['geoData']['coordinates'][1]
-        line_rast=math.sqrt((longitude-x)**2+(latitude-y)**2)
-        if(line_rast<rast):
-            closest=line
-            rast=line_rast
+    distance_to_bar = float('inf')
+    for json_line in data:
+        bar_longitude = json_line['Cells']['geoData']['coordinates'][0]
+        bar_latitude = json_line['Cells']['geoData']['coordinates'][1]
+        json_line_distance_to_bar = math.sqrt((longitude-bar_longitude)**2 +
+                                              (latitude-bar_latitude)**2)
+        if(json_line_distance_to_bar < distance_to_bar):
+            closest = json_line
+            distance_to_bar = json_line_distance_to_bar
     return closest
 
+
 if __name__ == '__main__':
-    file=load_data('data.json')
-    big_bar=get_biggest_bar(file)
-    small_bar=get_smallest_bar(file)
-    print("Biggest bar in Moscow: ", big_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), big_bar['Cells']['SeatsCount'])
-    print("Smallest bar in Moscow: ", small_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), small_bar['Cells']['SeatsCount'])
-    x=float(input("Input longitude: "))
-    y=float(input("Input latitude: "))
-    closest_bar=get_closest_bar(file, x, y)
-    print("Nearest bar: ", closest_bar['Cells']['Name'].encode(sys.stdout.encoding,'ignore').decode(sys.stdout.encoding), closest_bar['Cells']['geoData']['coordinates'][0], closest_bar['Cells']['geoData']['coordinates'][1])
+    if len(sys.argv) < 2:
+        print("Input path to the json file second argument")
+        exit()
+    filepath = sys.argv[1]
+    if not os.path.exists(filepath):
+        print("Incorrect path")
+        exit()
+    json_content = load_json_data(filepath)
+    biggest_bar = get_biggest_bar(json_content)
+    smallest_bar = get_smallest_bar(json_content)
+    print("Biggest bar: \n\t",
+          biggest_bar['Cells']['Name'].encode(
+              sys.stdout.encoding,
+              'ignore').decode(sys.stdout.encoding))
+    print("Smallest bar: \n\t",
+          smallest_bar['Cells']['Name'].encode(
+              sys.stdout.encoding,
+              'ignore').decode(sys.stdout.encoding))
+    longitude = float(input("Input longitude: "))
+    latitude = float(input("Input latitude: "))
+    closest_bar = get_closest_bar(json_content, longitude, latitude)
+    print("Closest bar: \n\t",
+          closest_bar['Cells']['Name'].encode(
+              sys.stdout.encoding,
+              'ignore').decode(sys.stdout.encoding))
